0f3b3dbbb2172cf949207b41eed55083ce0a0c91
Irenegm06 <irenetrabajos0@gmail.com>
2025-03-13 10:25:24 +0100

    Añadir nuevo metodo en date

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -19,12 +19,13 @@ public class Date {
   private final int day;
   private final int month;
   private final int year;
-
+  private final int tiempo;
   /**
    * Retrieves the day component of this date.
    *
    * @return The day of the month (1-31).
    */
+
   public int getDay() {
     return day;
   }
@@ -47,6 +48,7 @@ public class Date {
     return year;
   }
 
+  public int getTiempo () { return tiempo; }
   /**
    * Constructs a Date object representing a specific day, month, and year.
    *
@@ -55,10 +57,11 @@ public class Date {
    * @param year The year (should ideally be within the valid range of 1900 to 2050 for validation
    *     to pass).
    */
-  public Date(int day, int month, int year) {
+  public Date(int day, int month, int year, int tiempo) {
     this.day = day;
     this.month = month;
     this.year = year;
+    this.tiempo = tiempo;
   }
 
   /**
